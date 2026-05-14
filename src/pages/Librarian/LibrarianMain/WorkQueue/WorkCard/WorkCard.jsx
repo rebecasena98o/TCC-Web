58ada09574abce586c3ea7372eef1ab1666e365f
SleppyDoge <miguelcolares8@gmail.com>
2026-05-14 06:08:14 -0300

    feat/Added the WorkCard

---
+++ src/pages/Librarian/LibrarianMain/WorkQueue/WorkCard/WorkCard.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import '../../../../../Style/Librarian/StyleWorkCard.css'
+
+
+
+const statusConfig = {
+    "Pendente": {
+        color: "#fff",
+        bg: "rgb(106, 113, 129)",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+    },
+    "Em Correção": {
+        color: "#fff",
+        bg: "rgb(44, 126, 254)",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M320-440h320v-80H320v80Zm0 120h320v-80H320v80Zm0 120h200v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>
+    },
+    "Ajustes Necessários": {
+        color: "#fff",
+        bg: "rgb(240, 177, 1)",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M508.5-291.5Q520-303 520-320t-11.5-28.5Q497-360 480-360t-28.5 11.5Q440-337 440-320t11.5 28.5Q463-280 480-280t28.5-11.5ZM440-440h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+    },
+    "Aprovado": {
+        color: "#fff",
+        bg: "rgb(0, 201, 79)",
+        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+    },
+}
+
+function WorkCard({ ticket }) {
+    const [selected, setSelected] = useState(false)
+    const status = statusConfig[ticket.type] || statusConfig["Pendente"]
+
+    return (
+        <div 
+        className={`workCard ${selected ? 'workCardSelected' : ''}`}
+        onClick={() => setSelected(!selected)}
+        >
+            <div className="workCardUser">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM247-527q-47-47-47-113t47-113q47-47 113-47t113 47q47 47 47 113t-47 113q-47 47-113 47t-113-47Zm466 0q-47 47-113 47-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113q0 66-47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm296.5-343.5Q440-607 440-640t-23.5-56.5Q393-720 360-720t-56.5 23.5Q280-673 280-640t23.5 56.5Q327-560 360-560t56.5-23.5ZM360-240Zm0-400Z"/>
+                </svg>
+                <span>{ticket.user}</span>
+            </div>
+            <div className="workCardTitle">{ticket.title}</div>
+            <div className="workCardFooter">
+                <span
+                    className="workCardBadge"
+                    style={{ color: status.color, backgroundColor: status.bg }}
+                >
+                    <span className="workCardBadgeSvg">{status.svg}</span>
+                    {ticket.type}
+                </span>
+                <span className="workCardVersion">v{ticket.version}</span>
+            </div>
+        </div>
+    )
+}
+
+export default WorkCard
--- src/pages/Librarian/LibrarianQueue.jsx
+++ src/pages/Librarian/LibrarianQueue.jsx
@@ -1,7 +1,7 @@
 import '../../Style/Librarian/StyleLibrarianQueue.css'
 import Header from  '../../Components/Header.jsx'
-import StatsCards from './QuickStatsList/StatsCards/StatsCards.jsx'
 import QuickStatsList from './QuickStatsList/QuickStatsList.jsx'
+import WorkCard from './LibrarianMain/WorkQueue/WorkCard/WorkCard.jsx'
 
 function LibrarianQueue() {
 
@@ -10,37 +10,76 @@ function LibrarianQueue() {
         email: "admin@email.com"
     }
 
-const cardInfos = [
-    {
-        name: "Pendentes",
-        value: 1,
-        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>,
-        color: "#888"
-    },
-    {
-        name: "Em Correção",
-        value: 1,
-        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M320-440h320v-80H320v80Zm0 120h320v-80H320v80Zm0 120h200v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>,
-        color: "#2196F3"
-    },
-    {
-        name: "Ajustes Necessários",
-        value: 1,
-        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M508.5-291.5Q520-303 520-320t-11.5-28.5Q497-360 480-360t-28.5 11.5Q440-337 440-320t11.5 28.5Q463-280 480-280t28.5-11.5ZM440-440h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>,
-        color: "#FFC107"
-    },
-    {
-        name: "Aprovados",
-        value: 1,
-        svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>,
-        color: "#4CAF50"
-    },
-]
+    const cardInfos = [
+        {
+            name: "Pendentes",
+            value: 1,
+            svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>,
+            color: "#888"
+        },
+        {
+            name: "Em Correção",
+            value: 1,
+            svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M320-440h320v-80H320v80Zm0 120h320v-80H320v80Zm0 120h200v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>,
+            color: "#2196F3"
+        },
+        {
+            name: "Ajustes Necessários",
+            value: 1,
+            svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M508.5-291.5Q520-303 520-320t-11.5-28.5Q497-360 480-360t-28.5 11.5Q440-337 440-320t11.5 28.5Q463-280 480-280t28.5-11.5ZM440-440h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>,
+            color: "#FFC107"
+        },
+        {
+            name: "Aprovados",
+            value: 1,
+            svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>,
+            color: "#4CAF50"
+        },
+    ]
+
+    const tickets = [
+        {
+            user: "Maria Silva",
+            title: "Análise de Sistemas de Informação na Gestão Empresarial",
+            version: 2,
+            sentDate: "2026-04-15",
+            type: "AJUSTES"
+        },
+        {
+            user: "João Santos",
+            title: "Impactos da Inteligência Artificial no Mercado de Trabalho",
+            version: 1,
+            sentDate: "2026-04-28",
+            type: "PENDENTE"
+        },
+        {
+            user: "Ana Costa",
+            title: "Sustentabilidade e Responsabilidade Social Corporativa",
+            version: 1,
+            sentDate: "2026-04-27",
+            type: "CORRECAO"
+        },
+        {
+            user: "Pedro Oliveira",
+            title: "Marketing Digital e Comportamento do Consumidor",
+            version: 3,
+            sentDate: "2026-04-10",
+            type: "APROVADO"
+        }
+    ]
+
+    const testTicket = {
+        user: "Maria Silva",
+        title: "Análise de Sistemas de Informação na Gestão Empresarial",
+        type: "Ajustes Necessários",
+        version: 2
+    }
 
     return (
         <div className="librarianQueue">
         <Header user={userInfo}/>
-        <QuickStatsList cardInfos={cardInfos}/>
+        {/* <QuickStatsList cardInfos={cardInfos}/> */}
+        <WorkCard ticket={testTicket}/>
         </div>
     )
 }
