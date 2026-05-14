efca47cda1675ad8bf04a6c5d009f6bfe46c23cc
ALUNO K08 <alunok08@unifor.local>
2026-05-13 21:08:10 -0300

    adjust: adds delegation screen with transfer mode.

--- src/Routes/TccRoutesApp.jsx
+++ src/Routes/TccRoutesApp.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// Importamos apenas o Login para o teste
 import Login from '../pages/login';
+import AdminRoutes from './AdminRoutes';
 
 function TccRoutesApp({ setUser }) {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Qualquer caminho agora vai renderizar o Login */}
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        {AdminRoutes()}
         <Route path="*" element={<Login setUser={setUser} />} />
       </Routes>
     </BrowserRouter>
