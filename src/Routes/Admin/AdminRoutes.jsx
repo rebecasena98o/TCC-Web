98be385c53cf1fdee77e86190373b827d2ae9845
vitord25 <vitordantastenis@outlook.com>
2026-05-18 01:24:29 -0300

    feat: add Analytics page and update AdminRoutes

--- src/Routes/Admin/AdminRoutes.jsx
+++ src/Routes/Admin/AdminRoutes.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Analytics from '../../pages/Admin/Analytics';
 import Delegacao from '../../pages/Admin/Delegacao';
 
 const AdminRoutes = () => [
-  <Route key="delegacao" path="/admin/delegacao" element={<Delegacao />} />
+  <Route key="analytics" path="/admin/analytics" element={<Analytics />} />,
+  <Route key="delegacao" path="/admin/delegacao" element={<Delegacao />} />,
 ];
 
 export default AdminRoutes;
