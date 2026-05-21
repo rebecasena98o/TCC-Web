d4d1daec2a7d0d6bad704496a1b6a798bb0f4335
SleppyDoge <miguelcolares8@gmail.com>
2026-05-21 14:42:43 -0300

    Added the Button to acess AI support

--- src/pages/Librarian/LibrarianMain/TccAnalysis/TccAnalysis.jsx
+++ src/pages/Librarian/LibrarianMain/TccAnalysis/TccAnalysis.jsx
@@ -102,17 +102,23 @@ function TccAnalysis({ ticket }) {
 
                     <button className="tccBtnSave">Salvar Feedback</button>
 
-                    <div className="tccAnalysisBtnRow">
-                        <button className="tccBtnDownload">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
-                            Baixar TCC Original
-                        </button>
-                        <button className="tccBtnUpload">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
-                            Enviar Versão Corrigida
-                        </button>
+                        <div className="tccAnalysisBtnRow">
+                            <button className="tccBtnDownload">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+                                Baixar TCC Original
+                            </button>
+                            <button className="tccBtnUpload">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+                                Enviar Versão Corrigida
+                            </button>
+                            <button className="tccBtnIa">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                                    <path d="M160-360q-50 0-85-35t-35-85q0-50 35-85t85-35v-80q0-33 23.5-56.5T240-760h120q0-50 35-85t85-35q50 0 85 35t35 85h120q33 0 56.5 23.5T800-680v80q50 0 85 35t35 85q0 50-35 85t-85 35v160q0 33-23.5 56.5T720-120H240q-33 0-56.5-23.5T160-200v-160Zm242.5-97.5Q420-475 420-500t-17.5-42.5Q385-560 360-560t-42.5 17.5Q300-525 300-500t17.5 42.5Q335-440 360-440t42.5-17.5Zm240 0Q660-475 660-500t-17.5-42.5Q625-560 600-560t-42.5 17.5Q540-525 540-500t17.5 42.5Q575-440 600-440t42.5-17.5ZM320-280h320v-80H320v80Zm-80 80h480v-480H240v480Zm240-240Z"/>
+                                </svg>
+                                Auxilio IA
+                            </button>
+                        </div>
                     </div>
-                </div>
             )}
 
             {option === 'chat' && (
